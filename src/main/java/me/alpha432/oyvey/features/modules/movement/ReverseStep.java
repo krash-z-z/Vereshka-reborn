5b6a33c7d7e949e4cd354a0c7c16f71025ebc5fb
krash-z-z <krasher774@gmail.com>
2025-07-30 21:42:52 +0200

    Update ReverseStep.java

--- src/main/java/me/alpha432/oyvey/features/modules/movement/ReverseStep.java
+++ src/main/java/me/alpha432/oyvey/features/modules/movement/ReverseStep.java
@@ -2,15 +2,24 @@ package me.alpha432.oyvey.features.modules.movement;
 
 import me.alpha432.oyvey.features.modules.Module;
 
-public class ReverseStep extends Module {
-    public ReverseStep() {
-        super("ReverseStep", "step but reversed..", Category.MOVEMENT, true, false, false);
+public class speed extends Module {
+    public speed() {
+        super("szybki speed na metahvh", Category.MOVEMENT, true, false, false);
     }
 
     @Override
-    public void onUpdate() {
-        if (nullCheck()) return;
-        if (mc.player.isInLava() || mc.player.isTouchingWater() || !mc.player.isOnGround()) return;
-        mc.player.addVelocity(0, -1, 0);
+    public void onUpdate() 
+{
+        boolean longHop = this.LongHop.getBool() && (Minecraft.player.isJumping() || Minecraft.player.fallDistance != 0.0f);
+        boolean onGround = this.OnGround.getBool() && !Minecraft.player.isJumping() && Minecraft.player.onGround && Minecraft.player.isCollidedVertically && MoveMeHelp.getSpeed() < 0.9;
+        Speed.mc.timer.speed = 1.2;
+        if (longHop) {
+            Minecraft.player.jumpMovementFactor = 0.17f;
+            Minecraft.player.multiplyMotionXZ(1.005f);
+        }
+        if (onGround) {
+            Minecraft.player.multiplyMotionXZ(1.212f);
+        }
+    }
     }
 }
